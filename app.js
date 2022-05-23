f9408ca4ed8ae9618114eaeaae2d4557ab833add
Tauqeer Ahmad <hellotauqeer@gmail.com>
2022-05-23 08:24:55 +0530

    added root

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('root'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/html/index.html')
+    res.sendFile(__dirname + '/root/html/index.html')
   })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
