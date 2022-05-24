28eddfe0f5a76388339968c71cd850ac2b2f15d6
Tauqeer Ahmad <hellotauqeer@gmail.com>
2022-05-24 12:09:58 +0530

    added login.html

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const port = 3000
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/html/index.html')
-  })
+  res.sendFile(__dirname + '/public/html/index.html')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/html/login.html')
+})
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://127.0.0.1:${port}`)
 })
